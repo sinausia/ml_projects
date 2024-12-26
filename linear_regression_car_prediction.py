453a67275f135d6e7585977d07bb02587b351400
DSinausia <119315272+sinausia@users.noreply.github.com>
2024-12-26 12:45:56 +0200

    Create linear_regression_car_prediction.py

---
+++ linear_regression_car_prediction.py
@@ -0,0 +1,247 @@
+
+import pandas as pd
+import numpy as np
+
+
+data = 'https://raw.githubusercontent.com/alexeygrigorev/mlbookcamp-code/master/chapter-02-car-price/data.csv'
+
+
+#%% Data Preparation
+
+df = pd.read_csv('data.csv')
+df.columns = df.columns.str.lower().str.replace(' ', '_')
+df['make'].str.lower().str.replace(' ', '_')
+strings = list(df.dtypes[df.dtypes == 'object'].index)
+
+for col in strings:
+    df[col] = df[col].str.lower().str.replace(' ', '_')
+
+#%% Exploratory Data Analysis
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+for col in df.columns:
+    print(col)
+    print(df[col].unique()[:5])
+    print(df[col].nunique())
+    print()
+
+%matplotlib inline
+sns.histplot(df.msrp[df.msrp < 100000], bins=50) #shows long tail, transform to normal distribution with log
+price_logs = np.log1p(df.msrp)
+sns.histplot(price_logs, bins=50)
+df.isnull().sum()
+
+#%% Setting up the validation framework
+
+n = len(df)
+
+n_val = int(n * 0.2)
+n_test = int(n * 0.2)
+n_train = n - n_val - n_test
+
+idx = np.arange(n)
+np.random.seed(2) #to make it always repeatable
+np.random.shuffle(idx)
+
+df_train = df.iloc[idx[:n_train]]
+df_val = df.iloc[idx[n_train:n_train+n_val]]
+df_test = df.iloc[idx[n_train+n_val:]]
+
+df_train = df_train.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+y_train = np.log1p(df_train.msrp.values)
+y_val = np.log1p(df_val.msrp.values)
+y_test = np.log1p(df_test.msrp.values)
+
+del df_train['msrp']
+del df_val['msrp']
+del df_test['msrp']
+
+#%% linear regression
+
+def dot(xi, w):
+    n = len(xi)   
+    res = 0.0    
+    for j in range(n):
+        res = res + xi[j] * w[j]    
+    return res
+
+w_new = [w0] + w
+def linear_regression(xi):
+    xi = [1] + xi
+    return dot(xi, w_new)
+
+def linear_regression(X):
+    return X.dot(w_new)
+
+X = [
+    [148, 24, 1385],
+    [132, 25, 2031],
+    [453, 11, 86],
+    [158, 24, 185],
+    [172, 25, 201],
+    [413, 11, 86],
+    [38,  54, 185],
+    [142, 25, 431],
+    [453, 31, 86],
+]
+
+X = np.array(X)
+X
+
+#%% train linear regression
+
+def train_linear_regression(X, y):
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+
+    XTX = X.T.dot(X)
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)
+    
+    return w_full[0], w_full[1:]
+
+#%% car baseline model
+
+base = ['engine_hp', 'engine_cylinders', 'highway_mpg',
+        'city_mpg', 'popularity']  # quantitative variables
+
+X_train = df_train[base].fillna(0).values
+w0, w = train_linear_regression(X_train, y_train)
+y_pred = w0 + X_train.dot(w)
+
+#%% RMSE
+def rmse(y, y_pred):
+    se = (y - y_pred) ** 2
+    mse = se.mean()
+    return np.sqrt(mse)
+
+#%% validation
+
+def prepare_X(df):
+    df_num = df[base]
+    df_num = df_num.fillna(0)
+    X = df_num.values
+    return X
+X_train = prepare_X(df_train)
+w0, w = train_linear_regression(X_train, y_train)
+
+X_val = prepare_X(df_val)
+y_pred = w0 + X_val.dot(w)
+print(rmse(y_val, y_pred))
+
+#%% feature engineering
+def prepare_X(df):
+    df = df.copy()
+    df['age'] = 2017 - df['year'] #adding age variable
+    features = base + ['age']
+    df_num = df[features]
+    df_num = df_num.fillna(0)
+    X = df_num.values
+    return X
+
+X_train = prepare_X(df_train)
+w0, w = train_linear_regression(X_train, y_train)
+
+X_val = prepare_X(df_val)
+y_pred = w0 + X_val.dot(w)
+print(rmse(y_val, y_pred))
+
+#%% adding categorical variables
+
+categorical_columns = [
+    'make', 'model', 'engine_fuel_type', 'driven_wheels', 'market_category',
+    'vehicle_size', 'vehicle_style']
+
+categorical = {}
+
+for c in categorical_columns:
+    categorical[c] = list(df_train[c].value_counts().head().index)
+
+def prepare_X(df):
+    df = df.copy()
+    
+    df['age'] = 2017 - df['year']
+    features = base + ['age']
+    for v in [2, 3, 4]:
+        df['num_doors_%d' % v] = (df.number_of_doors == v).astype(int)
+        features.append('num_doors_%d' % v)
+    for name, values in categorical.items():
+        for value in values:
+            df['%s_%s' % (name, value)] = (df[name] == value).astype(int)
+            features.append('%s_%s' % (name, value))
+    df_num = df[features]
+    df_num = df_num.fillna(0)
+    X = df_num.values
+    return X
+
+X_train = prepare_X(df_train)
+w0, w = train_linear_regression(X_train, y_train)
+
+X_val = prepare_X(df_val)
+y_pred = w0 + X_val.dot(w)
+print(rmse(y_val, y_pred))
+
+#%% regularization
+
+def train_linear_regression_reg(X, y, r=0.001):
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+
+    XTX = X.T.dot(X)
+    XTX = XTX + r * np.eye(XTX.shape[0])
+
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)   
+    return w_full[0], w_full[1:]
+
+X_train = prepare_X(df_train)
+w0, w = train_linear_regression_reg(X_train, y_train, r=0.01)
+
+X_val = prepare_X(df_val)
+y_pred = w0 + X_val.dot(w)
+print(rmse(y_val, y_pred))
+
+#%% tuning the model
+
+for r in [0.0, 0.00001, 0.0001, 0.001, 0.1, 1, 10]: # find best value for r
+    X_train = prepare_X(df_train)
+    w0, w = train_linear_regression_reg(X_train, y_train, r=r)
+    X_val = prepare_X(df_val)
+    y_pred = w0 + X_val.dot(w)
+    score = rmse(y_val, y_pred)    
+    print(r, w0, score)
+
+r = 0.001
+X_train = prepare_X(df_train)
+w0, w = train_linear_regression_reg(X_train, y_train, r=r)
+
+X_val = prepare_X(df_val)
+y_pred = w0 + X_val.dot(w)
+score = rmse(y_val, y_pred)
+
+#%% Using the model
+
+df_full_train = pd.concat([df_train, df_val])
+df_full_train = df_full_train.reset_index(drop=True)
+X_full_train = prepare_X(df_full_train)
+y_full_train = np.concatenate([y_train, y_val])
+w0, w = train_linear_regression_reg(X_full_train, y_full_train, r=0.001)
+X_test = prepare_X(df_test)
+y_pred = w0 + X_test.dot(w)
+score = rmse(y_test, y_pred)
+
+# using an unseen car from the df to "predict"
+car = df_test.iloc[20].to_dict()
+df_small = pd.DataFrame([car])
+
+X_small = prepare_X(df_small)
+y_pred = w0 + X_small.dot(w)
+y_pred = y_pred[0]
+
+print(np.expm1(y_pred))
+print(np.expm1(y_test[20]))
